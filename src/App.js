523e33cc01bdea949335f5576980ab00337d677f
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2024-11-11 15:06:41 +0530

    data binded to datagrid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { DataGrid } from '@mui/x-data-grid';
 
 function App() {
+  let details = [
+    {
+      id: 1,
+      name: "Rajesh",
+      age: 33,
+      location: "Banglore"
+    },
+    {
+      id: 2,
+      name: "Nagarjun",
+      age: 23,
+      location: "Mysore"
+    },
+    {
+      id: 3,
+      name: "Girish",
+      age: 25,
+      location: "Mandya"
+    },
+    {
+      id: 4,
+      name: "Ramanath",
+      age: 24,
+      location: "Honnavara"
+    },
+    {
+      id: 5,
+      name: "Vinay",
+      age: 23,
+      location: "Udupi"
+    }
+  ];
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'age', headerName: 'Age', width: 120 },
+    { field: 'location', headerName: 'Location', width: 180 }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid rows={details} columns={columns} pageSize={5} />
     </div>
   );
 }
