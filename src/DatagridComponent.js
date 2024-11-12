170ece579f03a82f25eb75cb0b4ca3660dbb4114
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2024-11-12 15:00:38 +0530

    Update DatagridComponent.js

--- src/DatagridComponent.js
+++ src/DatagridComponent.js
@@ -3,14 +3,16 @@ import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
 const DatagridComponent = ({ rows, columns, onViewClick }) => {
+  
   return (
     <div style={{ height: 400, width: 650 }}>
       <DataGrid 
         rows={rows} 
         columns={columns} 
         pageSize={5} 
-        onRowClick={(params) => onViewClick(params.row)} 
+        // onRowClick={(params) => onViewClick(params.row)} 
       />
+    
     </div>
   );
 };
